bffb2b309520dd8973bd795db98d3421d42ec693
gauravmalaviya879 <malaviyagaurav879@gmail.com>
2024-08-22 15:56:20 +0530

    update footer - gaurav dev

--- src/assets/layouts/Footer.jsx
+++ src/assets/layouts/Footer.jsx
@@ -2,23 +2,33 @@ import React from "react";
 import { FiInstagram } from "react-icons/fi";
 import { FaFacebookF } from "react-icons/fa6";
 import { FaTwitter } from "react-icons/fa6";
+import footerlogo from "../images/footer-logo.png"
 
 const Footer = () => {
   return (
     <section className="">
-      <div className="w-[80%] m-auto py-11">
+      <div className="w-[90%] m-auto py-11">
         <div className="flex lg:flex-row flex-col">
-          <div className="lg:w-1/3 w-[100%]">
-            <h3 className=" py-3  font-thin md:w-[35%] w-[40%] font-WorkSansReg text-[20px] md:text-[25px] uppercase">
-              LA MEILLEUR MAMAN
+          <div className="lg:w-1/4 px-2 w-[100%]">
+
+
+          <div className="flex lg:justify-start  justify-center">
+             <img src={footerlogo} className="lg:w-[80%] md:w-[30%] md:h-[30%] w-[40%] h-[40%] lg:h-[80%] object-cover" alt="" />
+          </div> 
+
+          </div>
+          <div className="lg:w-1/4 px-2 w-[100%]">
+            <h3 className=" py-3  font-lora md:w-[60%] w-[50%]  text-[20px] md:text-[25px] ">
+            Les petits
+            appetits
             </h3>
-            <p className=" py-3  font-light  font-WorkSansReg text-[12px] md:text-[16px] ">
+            <p className=" py-3 font-light  font-WorkSansReg text-[12px] md:text-[16px] ">
               est un service de traiteur haut de gamme, exclusivement dédié à la
               fourniture de repas gastronomiques faits maison, frais et sains
               pour les écoles, les garderies,{" "}
             </p>
           </div>
-          <div className="lg:w-1/3 w-[100%] ">
+          <div className="lg:w-1/4 px-2 w-[100%] ">
             <div className="lg:mx-3">
               <h3 className=" py-3  font-bold  w-[100%] font-WorkSansReg text-[12px] md:text-[16px] uppercase">
                 nous sommes a <br></br>Montreal
@@ -35,7 +45,7 @@ const Footer = () => {
               </p>
             </div>
           </div>
-          <div className="lg:w-1/3 w-[100%] ">
+          <div className="lg:w-1/4 px-2 w-[100%] ">
             <div className="">
               <h3 className=" py-3  font-bold md:w-[100%] font-WorkSansReg text-[12px] md:text-[16px] uppercase">
                 heures d ‘ouvertures
