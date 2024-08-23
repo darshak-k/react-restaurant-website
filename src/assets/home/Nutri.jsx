331e586d69891106f3b38388f41c41da612e8713
gauravmalaviya879 <malaviyagaurav879@gmail.com>
2024-08-23 12:35:20 +0530

    second compnets - gaurav dev

--- src/assets/home/Nutri.jsx
+++ src/assets/home/Nutri.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import foodLunch from "../images/food-lunch.jpeg";
+import { useTranslation } from "react-i18next";
 
 const Nutri = () => {
+  const { t } = useTranslation();
   return (
     <div className="lg:my-11">
       <h4 className="text-center my-4 text-hot-pink font-WorkSansBold text-[14px] md:text-[18px] uppercase">
-        Des repas équilibrés et nutritifs fournis chaque jour
+        {t("layouttwocontectone")}
       </h4>
       <section className="w-[95%] m-auto">
         <div className="flex lg:flex-row flex-col-reverse ">
@@ -13,17 +15,13 @@ const Nutri = () => {
             <div className="w-[80%]">
               <div className=" lg:min-h-[400px] flex-col flex justify-center">
                 <h4 className=" py-3 text-green-food font-WorkSansBold text-[14px] md:text-[18px] uppercase">
-                  Programme de repas pour garderie ou Ecole
+                  {t("layouttwocontecttwo")}
                 </h4>
                 <p className="py-3 leading-snug text-hot-pink font-WorkSansBold text-[14px] md:text-[18px] uppercase">
-                  Nos repas sont préparés chaque jour avec des ingrédients
-                  frais, en tenant compte des besoins nutritionnels et des
-                  préférences gustatives des enfants
+                  {t("layouttwocontectthree")}
                 </p>
                 <p className="py-3 leading-snug text-hot-pink font-WorkSansBold text-[14px] md:text-[18px] uppercase">
-                  Des repas maison équilibrés, délicieux et abordables pour les
-                  garderies privées, publiques (CPE) et familiales de la région
-                  du Grand Montréal.
+                  {t("layouttwocontectfour")}
                 </p>
               </div>
             </div>
@@ -32,7 +30,11 @@ const Nutri = () => {
             <div className="lg:h-[400px] h-auto my-4 lg:my-0 overflow-hidden">
               <div className="flex justify-center">
                 <div className="lg:h-[550px] lg:w-[550px]  lg:top-[-74px] sm:h-[400px] h-[300px] w-[300px] sm:w-[400px] overflow-hidden relative rounded-full ">
-                  <img src={foodLunch} className="h-[100%] object-cover" alt="" />
+                  <img
+                    src={foodLunch}
+                    className="h-[100%] object-cover"
+                    alt=""
+                  />
                 </div>
               </div>
             </div>
